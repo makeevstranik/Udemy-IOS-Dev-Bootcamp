67e4b77445703a442d9b5c61bfd959562bdb9e08
makeev <makeevstranik@gmail.com>
2022-01-08 20:23:11 +0500

    #10.109

---
+++ Destini/Destini-iOS13/Model/ButtonChoice.swift
@@ -0,0 +1,13 @@
+//
+//  ButtonChoice.swift
+//  Destini-iOS13
+//
+//  Created by Евгений Макеев on 08.01.2022.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+enum ButtonChoice: Int {
+    case choice1 = 1
+    case choice2 = 2
+}
